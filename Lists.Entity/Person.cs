731551ff906878b7b83403ecc330e233795276bc
Johannes Ruhsam <johannes.ruhsam@gmx.at>
2019-10-23 19:03:13 +0200

    rewrite List to Generic List, changed two tests, set rest of tests as comments

---
+++ Lists.Entity/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace Lists.Entity
+{
+    public class Person : IComparable
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public int Age { get; set; }
+
+        public int Compare(object x, object y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName}";
+        }
+
+        //nested class z.B. SortAsc
+    }
+}
